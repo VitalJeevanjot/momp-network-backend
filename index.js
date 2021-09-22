5eb24047c5c502493d4ba625f29766e1490230e7
Genievot <40867747+genievot@users.noreply.github.com>
2021-09-22 14:31:31 +0530

    changes

--- index.js
+++ index.js
@@ -8,20 +8,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors())
 
-
 const { Universal, Node, MemoryAccount, Ae } = require('@aeternity/aepp-sdk');
 const smtp = require('./smtp_module.js')
 const __path = require('path');
 require('dotenv').config({path:__path.resolve(__dirname, ".env")})
+var fs = require('fs');
 
-const momp_CONTRACT_SOURCE = contractUtils.getContractContent('./contracts/merged_momp.aes');
+let momp_CONTRACT_SOURCE = fs.readFileSync('./contracts/merged_momp.aes', 'utf-8')
+
 let momp_contract = null
+
+let url = "https://testnet.aeternity.io"
+let Compilerurl = "https://latest.compiler.aepps.com"
 
 const keyPair = { // SUPER SECRET, CREATE ENV BEFORE SENDING TO GITHUB... BY JEEVANJOT
   "publicKey": process.env.OWNER_PUBLIC_KEY,
   "secretKey": process.env.OWNER_PRIVATE_KEY
 }
 
+var client_node = null
 async function initNode () {
   client_node = await Universal({
     nodes: [
@@ -36,7 +41,9 @@ async function initNode () {
     compilerUrl: Compilerurl
   });
 
-  momp_contract = await client.getContractInstance(momp_CONTRACT_SOURCE, { contractAddress: "ct_2Wjd1vVHYLzJdU3UiBcogpb7vCUryrCPqmQy2Dn94g8kHBer7L" });
+
+  momp_contract = await client_node.getContractInstance(momp_CONTRACT_SOURCE, { contractAddress: process.env.MOMP_ADDRESS });
+
 
 
 }
@@ -47,15 +54,22 @@ initNode()
 app.post('/register', async (req, res) => {
 
   console.log("Registration ----------------------------")
-  var fees_status = await momp_contract.methods.get_registration_fee_paid_or_not(get_sha256_in_bytes(req.body.user_email))
+
+  console.log("Email:- " + req.body.user_email)
+  console.log("Public Key:- " + req.body.public_key)
+  var fees_status = null
+  try {
+    console.log("Checking fee status.")
+    fees_status = await momp_contract.methods.get_registration_fee_paid_or_not(get_sha256_in_bytes(req.body.user_email))
+  } catch (e) {
+    console.log (e)
+    res.send("r0009: Contract call failed")
+    return
+  }
   
   console.log("Fee paid by " + req.body.user_email + " : ")
   console.log(fees_status.decodedResult)
 
-  if(!fees_status.decodedResult) {
-    res.send("ba0001: Register fees not paid!")
-    return
-  }
 
   function get_sha256 (val) {
     return crypto.createHash('sha256').update(val).digest('hex')
@@ -83,37 +97,48 @@ app.post('/register', async (req, res) => {
   }
 
   let otp_original = crypto.randomBytes(4).toString("hex");
-  let created_otp = get_sha256_in_bytes(get_sha256("jeevanjotsingh@yandex.com").toUpperCase() + otp_original + second_user)
+  let created_otp = get_sha256_in_bytes(get_sha256("jeevanjotsingh@yandex.com").toUpperCase() + otp_original + this.public_key)
   console.log("Otp created in bytes: " + created_otp)
 
   // Register account ...
-  console.log("Account registration:-------")
-  console.log("Email:- " + req.body.user_email)
-  console.log("Public Key:- " + req.body.public_key)
+  console.log("Account registration with:-")
   console.log("Otp original:- " + created_otp)
   console.log("Otp plain:- " + created_otp)
   
   try {
+    console.log("Registering account.")
     var register_account = await momp_contract.methods.add_email(get_sha256_in_bytes(req.body.user_email), req.body.public_key, created_otp)
     console.log(register_account.decodedResult)
   } catch (e) {
     console.log(e)
-    res.send("ba0010: " + "Error in register transaction")
+    res.send("w0001: Error in register transaction")
     return
   }
+
+  var __public_key = req.body.public_key
+  // try {
+  //   console.log("Getting client public key.")
+
+  //   try {
+  //     console.log("Getting client public key from new state.")
+  //     __public_key = await momp_contract.methods.clients_new_pub_key(get_sha256_in_bytes(req.body.user_email))
+  //   } catch (e) {
+  //     console.log("Getting client public key from stable state.")
+  //     console.log(e)
+  //     __public_key = await momp_contract.methods.clients_pub_key(get_sha256_in_bytes(req.body.user_email))
+  //   }
+  // } catch (e) {
+  //   console.log (e)
+  //   res.send("r0010: Contract call failed")
+  //   return
+  // }
   let args = [
     'MOMP Network',
     req.body.user_email,
-    "About added, your original OTP",
-    `
-    <h1 class="code-line" data-line-start=0 data-line-end=1 ><a id="Your_account_is_added_into_the_blockchain_0"></a>Your account is added into the blockchain!</h1>
-    <p class="has-line-data" data-line-start="2" data-line-end="3">Your account is added into the blockchain network. You need to verify your account to withdraw any existing asset from smart contract under your email or to use it further. To verify account use this OTP : <b>${otp_original}</b> at the website’s confirmation step.</p>
-    <blockquote>
-    <p class="has-line-data" data-line-start="4" data-line-end="5">This OTP will never expire for this account, You can verify this OTP at any time using MOMP website.</p>
-    </blockquote>
-    `
+    "About added, your original OTP"
   ]
   try {
+    console.log("Sending Email...")
     // result = "myResultByJeevanjot"
     result = await smtp.main({ _sender: args[0], _to: args[1], _subject: args[2] }, __public_key, otp_original)
   } catch (e) {
@@ -121,7 +146,7 @@ app.post('/register', async (req, res) => {
     result = "error: Wrong values!"
   }
   console.log("---------------------------- Registration")
-  res.send(req.body)
+  res.send(result)
 })
 
 app.listen(port, () => {
